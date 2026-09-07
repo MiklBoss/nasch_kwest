98e8459c4597c01747042c05c3f6affb5560e2a6
MiklBoss <mikl13@seznam.cz>
2026-09-08 00:52:43 +0200

    Update FAMILYQUEST_API URL in config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Update only this URL after deploying the two Apps Script files.
-window.FAMILYQUEST_API = 'https://script.google.com/macros/s/AKfycbyI_y7ugBpNBu___hq5jw2TBz5wWa0g_PlPU5L2v1I2ecXrcTZWDtCFFe3RoCu4T_4/exec';
+window.FAMILYQUEST_API = 'https://script.google.com/macros/s/AKfycbwu8HkSj84c7Ge678br4N5ydhyY_CZMpBXZsJ9dwRstSG1XWOSmGT9iDCzYkSN-tJLZ/exec';
